79b8cdfe75b72c8da6e9f79e3fd962b120952fc6
Cosmin Sergiu <Cosmin Sergiu>
2017-09-09 17:53:09 +0300

    Add project files.

---
+++ RATBVMvvmCross/RATBVMvvmCross.WPF/Views/FirstView.xaml.cs
@@ -0,0 +1,12 @@
+using MvvmCross.Wpf.Views;
+
+namespace RATBVMvvmCross.WPF.Views
+{
+    public partial class FirstView : MvxWpfView
+    {
+        public FirstView()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
